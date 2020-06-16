760ae4e1d67dddf9083bc724b409bc1e27a0de4d
Colleen Treado <treado.c@gmail.com>
2020-06-16 12:37:37 -0400

    Fixing download script

--- EJsAnalysis/scripts/download_grid.sh
+++ EJsAnalysis/scripts/download_grid.sh
@@ -15,36 +15,36 @@ types="tree"
 outpath="/eos/atlas/user/c/ctreado/EmergingJets/analysis_ntuples/gridOutput/v0_2020-06_mod/"
 
 ## data
-cont15="user.ctreado.data15_13TeV.*.EJsAna."${dtag_d}${vtag_d}"*.root"
-cont16="user.ctreado.data16_13TeV.*.EJsAna."${dtag_d}${vtag_d}"*.root"
-cont17="user.ctreado.data17_13TeV.*.EJsAna."${dtag_d}${vtag_d}"*.root"
-cont18="user.ctreado.data18_13TeV.*.EJsAna."${dtag_d}${vtag_d}"*.root"
+cont15="user.ctreado.data15_13TeV.*.EJsAna."${dtag_d}"."${vtag_d}"*.root"
+cont16="user.ctreado.data16_13TeV.*.EJsAna."${dtag_d}"."${vtag_d}"*.root"
+cont17="user.ctreado.data17_13TeV.*.EJsAna."${dtag_d}"."${vtag_d}"*.root"
+cont18="user.ctreado.data18_13TeV.*.EJsAna."${dtag_d}"."${vtag_d}"*.root"
 
 ## mc16
 # --> signal
-conta36="user.ctreado.mc16_13TeV.3120*.*r10740*.EJsAna.sub36."${dtag_s}${vtag_s}"*.root"
-contd36="user.ctreado.mc16_13TeV.3120*.*r10739*.EJsAna.sub36."${dtag_s}${vtag_s}"*.root" # change dtag_s for next 21 points
-conte36="user.ctreado.mc16_13TeV.3120*.*r11265*.EJsAna.sub36."${dtag_s}${vtag_s}"*.root"
+conta36="user.ctreado.mc16_13TeV.3120*.*r10740*.EJsAna.sub36."${dtag_s}"."${vtag_s}"*.root"
+contd36="user.ctreado.mc16_13TeV.3120*.*r10739*.EJsAna.sub36."${dtag_s}"."${vtag_s}"*.root" # change dtag_s for next 21 points
+conte36="user.ctreado.mc16_13TeV.3120*.*r11265*.EJsAna.sub36."${dtag_s}"."${vtag_s}"*.root"
 # --> background
-contab="user.ctreado.mc16_13TeV.361024.*r10847*.EJsAna.bkgd."${dtag_b}${vtag_b}"*.root"
-contdb="user.ctreado.mc16_13TeV.361024.*r10848*.EJsAna.bkgd."${dtag_b}${vtag_b}"*.root"
+contab="user.ctreado.mc16_13TeV.361024.*r10847*.EJsAna.bkgd."${dtag_b}"."${vtag_b}"*.root"
+contdb="user.ctreado.mc16_13TeV.361024.*r10848*.EJsAna.bkgd."${dtag_b}"."${vtag_b}"*.root"
 
 
 ## to download subset of files
-#downloadAndMergeEJs.py --container ${cont15} --types ${types} --outPath ${outpath} --nrandom 1
-#downloadAndMergeEJs.py --container ${cont16} --types ${types} --outPath ${outpath} --nrandom 1
-#downloadAndMergeEJs.py --container ${cont17} --types ${types} --outPath ${outpath} --nrandom 1 # --> downloads one from every period...
-#downloadAndMergeEJs.py --container ${cont18} --types ${types} --outPath ${outpath} --nrandom 1
-#downloadAndMergeEJs.py --container ${conta0} --types ${types} --outPath ${outpath} --nrandom 1
-#downloadAndMergeEJs.py --container ${contd0} --types ${types} --outPath ${outpath} --nrandom 1
-#downloadAndMergeEJs.py --container ${conte0} --types ${types} --outPath ${outpath} --nrandom 1
-#downloadAndMergeEJs.py --container ${contab} --types ${types} --outPath ${outpath} --nrandom 3
-#downloadAndMergeEJs.py --container ${contdb} --types ${types} --outPath ${outpath} --nrandom 3
+#downloadAndMergeEJs.py --container ${cont15}  --types ${types} --outPath ${outpath} --nrandom 1
+#downloadAndMergeEJs.py --container ${cont16}  --types ${types} --outPath ${outpath} --nrandom 1
+#downloadAndMergeEJs.py --container ${cont17}  --types ${types} --outPath ${outpath} --nrandom 1 # --> downloads one from every period...
+#downloadAndMergeEJs.py --container ${cont18}  --types ${types} --outPath ${outpath} --nrandom 1
+#downloadAndMergeEJs.py --container ${conta36} --types ${types} --outPath ${outpath} --nrandom 1
+#downloadAndMergeEJs.py --container ${contd36} --types ${types} --outPath ${outpath} --nrandom 1
+#downloadAndMergeEJs.py --container ${conte36} --types ${types} --outPath ${outpath} --nrandom 1
+#downloadAndMergeEJs.py --container ${contab}  --types ${types} --outPath ${outpath} --nrandom 3
+#downloadAndMergeEJs.py --container ${contdb}  --types ${types} --outPath ${outpath} --nrandom 3
 # --> can't download full datasets locally (too large); instead use above modified script to download specified number of files per sample
 
 
 ## to download full datasets
-downloadAndMergeEJs.py --container ${contd0} --types ${types} --outPath ${outpath}
+downloadAndMergeEJs.py --container ${contd36} --types ${types} --outPath ${outpath}
 
 
 
