ea1e29d33ca4ee32d2e8c8c86874dbc03acb2822
Colleen Treado <treado.c@gmail.com>
2020-06-16 11:02:58 -0400

    Updating download script for new ntuples

--- EJsAnalysis/scripts/download_grid.sh
+++ EJsAnalysis/scripts/download_grid.sh
@@ -1,57 +1,50 @@
 #!/bin/bash
 # script for getting analysis output from grid
 
-## --> CHANGE ACCORDINGLY; CHANGE OUTPATH TO "LATEST"; REMOVE HOUR STRING FROM CONTAINER NAME
-#otag="TEST"
-#dtag="2019.12.31"
-#vtag="v0"
+# old dtag="2020-01-08" (or "2020-01-23" for one mc sample), vtag="v0"
 
-#types="cutflow,hist-output,metadata,tree"
-#outpath="$EJ_PATH/../output/gridOutput/tmp_test/"
-
-## --> ADD DATA18/7/6/5, MC16A/D/E (SUB0,SUB1)
-### data
-#cont17="user.ctreado.data17_13TeV.*.EJsAna."${otag}"."${dtag}"_12h."${vtag}"*.root"
-
-### mc16
-#contd0="user.ctreado.mc16_13TeV.3120*.*r10739*.EJsAna.sub0."${otag}"."${dtag}"_12h."${vtag}"*.root"
-
-##downloadAndMerge.py --container ${cont17} --types ${types} --outPath ${outpath}
-#downloadAndMerge.py --container ${contd0} --types ${types} --outPath ${outpath}
-
-
-dtag="2020-01"
-vtag="v0"
+dtag_d="2020-06-15" # update
+vtag_d="v1"
+dtag_s="2020-06-15"
+vtag_s="v1"
+dtag_b="2020-06-15" # update
+vtag_b="v1"
 
 types="tree"
-outpath="$EJ_PATH/../output/gridOutput/v0_2020-01_n1/"
+#outpath="$EJ_PATH/../output/gridOutput/v0_2020-06_mod/"
+outpath="/eos/atlas/user/c/ctreado/EmergingJets/analysis_ntuples/gridOutput/v0_2020-06_mod/"
 
 ## data
-cont15="user.ctreado.data15_13TeV.*.EJsAna."${dtag}"-08."${vtag}"*.root"
-cont16="user.ctreado.data16_13TeV.*.EJsAna."${dtag}"-08."${vtag}"*.root"
-cont17="user.ctreado.data17_13TeV.*.EJsAna."${dtag}"-08."${vtag}"*.root"
-cont18="user.ctreado.data18_13TeV.*.EJsAna."${dtag}"-08."${vtag}"*.root"
+cont15="user.ctreado.data15_13TeV.*.EJsAna."${dtag_d}${vtag_d}"*.root"
+cont16="user.ctreado.data16_13TeV.*.EJsAna."${dtag_d}${vtag_d}"*.root"
+cont17="user.ctreado.data17_13TeV.*.EJsAna."${dtag_d}${vtag_d}"*.root"
+cont18="user.ctreado.data18_13TeV.*.EJsAna."${dtag_d}${vtag_d}"*.root"
 
 ## mc16
 # --> signal
-conta0="user.ctreado.mc16_13TeV.3120*.*r10740*.EJsAna.sub0."${dtag}"*."${vtag}"*.root"
-contd0="user.ctreado.mc16_13TeV.3120*.*r10739*.EJsAna.sub0."${dtag}"*."${vtag}"*.root"
-conte0="user.ctreado.mc16_13TeV.3120*.*r11265*.EJsAna.sub0."${dtag}"*."${vtag}"*.root"
+conta36="user.ctreado.mc16_13TeV.3120*.*r10740*.EJsAna.sub36."${dtag_s}${vtag_s}"*.root"
+contd36="user.ctreado.mc16_13TeV.3120*.*r10739*.EJsAna.sub36."${dtag_s}${vtag_s}"*.root" # change dtag_s for next 21 points
+conte36="user.ctreado.mc16_13TeV.3120*.*r11265*.EJsAna.sub36."${dtag_s}${vtag_s}"*.root"
 # --> background
-contab="user.ctreado.mc16_13TeV.361024.*r10847*.EJsAna.bkgd."${dtag}"-08."${vtag}"*.root"
-contdb="user.ctreado.mc16_13TeV.361024.*r10848*.EJsAna.bkgd."${dtag}"-08."${vtag}"*.root"
+contab="user.ctreado.mc16_13TeV.361024.*r10847*.EJsAna.bkgd."${dtag_b}${vtag_b}"*.root"
+contdb="user.ctreado.mc16_13TeV.361024.*r10848*.EJsAna.bkgd."${dtag_b}${vtag_b}"*.root"
 
+
+## to download subset of files
 #downloadAndMergeEJs.py --container ${cont15} --types ${types} --outPath ${outpath} --nrandom 1
 #downloadAndMergeEJs.py --container ${cont16} --types ${types} --outPath ${outpath} --nrandom 1
 #downloadAndMergeEJs.py --container ${cont17} --types ${types} --outPath ${outpath} --nrandom 1 # --> downloads one from every period...
 #downloadAndMergeEJs.py --container ${cont18} --types ${types} --outPath ${outpath} --nrandom 1
 #downloadAndMergeEJs.py --container ${conta0} --types ${types} --outPath ${outpath} --nrandom 1
-downloadAndMergeEJs.py --container ${contd0} --types ${types} --outPath ${outpath} --nrandom 1
+#downloadAndMergeEJs.py --container ${contd0} --types ${types} --outPath ${outpath} --nrandom 1
 #downloadAndMergeEJs.py --container ${conte0} --types ${types} --outPath ${outpath} --nrandom 1
 #downloadAndMergeEJs.py --container ${contab} --types ${types} --outPath ${outpath} --nrandom 3
 #downloadAndMergeEJs.py --container ${contdb} --types ${types} --outPath ${outpath} --nrandom 3
 # --> can't download full datasets locally (too large); instead use above modified script to download specified number of files per sample
-# --> CHECK NRANDOM FOR DATA AND BKGD...
+
+
+## to download full datasets
+downloadAndMergeEJs.py --container ${contd0} --types ${types} --outPath ${outpath}
 
 
 
